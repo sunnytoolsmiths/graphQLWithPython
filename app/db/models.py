25e8e3cb5117f67cd2410ffefc8ba2c1c1feafaf
Sunny Xia <sunnyxia@Sunnys-iMac.local>
2024-04-02 19:59:37 -0700

    second change

---
+++ app/db/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer,String, ForeignKey
+from sqlalchemy.orm import relationship
+
+Base = declarative_base
+
+class Employer(Base):
+    __tablename__ = "employers"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    contact_email = Column(String)
+    industry = Column(String)
+    employer_id = Column(Integer, ForeignKey("employers.id"))
+    jobs = relationship ("Job", back_populates="employer")
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    description =  Column(String)
+    employer_id = Column(Integer, ForeignKey("employers.id"))
+    employer = relationship("Employer", back_populates="jobs")
+
+   
