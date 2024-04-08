3319b7eb437be77a4b6a8ce571a1599a9f454b54
Sunny Xia <sunnyxia@Sunnys-iMac.local>
2024-04-07 20:39:18 -0700

    homework2

--- app/gql/queries.py
+++ app/gql/queries.py
@@ -3,6 +3,8 @@ from app.gql.types import JobObject, EmployerObject
 from app.db.data import employers_data, jobs_data
 from app.db.database import retrive_data
 from app.db.models import Job, Employer
+from sqlalchemy.orm import joinedload
+from app.db.database import Session
 
 class Query(ObjectType):
     jobs = List(JobObject)
@@ -10,7 +12,8 @@ class Query(ObjectType):
 
     @staticmethod
     def resolve_jobs (root, info):
-        return retrive_data(Job)
+        #return retrive_data(Job)
+        return Session().query(Job).options(joinedload(Job.employer)).all()
     
     def resolve_employers (root, info):
         return retrive_data(Employer)
